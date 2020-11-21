b338847342add8b87158f8bc0bd236e5ad965acc
HemiYbarra <60747748+HemiYbarra@users.noreply.github.com>
2020-11-22 00:19:30 +0300

    Add files via upload

---
+++ AboutMe.py
@@ -0,0 +1,16 @@
+myGreeting = "Всем привет,"
+myName = "Дмитрий"
+mySurname = "Иванов,"
+myAge = 17
+myBorn = "Великий Новгород,"
+myStudy = "Строительном колледже."
+myLive = "Чудово."
+myHobby = "программированием."
+myRareHobby = 1.5
+myAccess = "так как доступ к компьютеру у меня есть только на выходных днях."
+myActivity = "путешествовать,"
+difCountries = 3
+print(myGreeting, "меня зовут", myName, mySurname, "мне", myAge, "лет.", "Я родился в городе",  myBorn, "учусь в",
+      myStudy, "В данный момент живу в городе", myLive, "В свободное время занимаюсь", myHobby,
+      "Но к сожалению", "программированием я могу заниматься только", myRareHobby, "часа в неделю,",
+      myAccess, "Ещё я люблю", myActivity, "помимо России, я был ещё в", difCountries, "странах.")
